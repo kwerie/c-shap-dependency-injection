8f3a44392bdfd707d7460f7ac5ac3b905e2feb71
Rico Hermsen <rico@jrny.nl>
2024-04-05 15:24:47 +0200

    Refactored the Configuration.cs so It would work on my machine

--- 02/demos/2.2 BeforeDI/Module2.BeforeDI/Program.cs
+++ 02/demos/2.2 BeforeDI/Module2.BeforeDI/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("File cannot be empty");
+﻿using Module2.BeforeDI;
+using Module2.BeforeDI.Shared;
+using Module2.BeforeDI.Source;
+using Module2.BeforeDI.Target;
+
+internal class Program
+{
+    public static int Main()
+    {
+        var configuration = new Configuration();
+        var priceParser = new PriceParser();
+        var productSource = new ProductSource(
+            configuration,
+            priceParser
+        );
+        var productFormatter = new ProductFormatter();
+        var productTarget = new ProductTarget(
+            configuration,
+            productFormatter
+        );
+        var productImporter = new ProductImporter(productSource, productTarget);
+        productImporter.Run();
+        return 0;
+    }
+}
--- 02/demos/2.2 BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
+++ 02/demos/2.2 BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
@@ -3,7 +3,13 @@ public class Configuration
 {
     // We will deal with passing in configuration in a better way in a future module
     // For now hardcoding the values is enough to practice with the concepts from this module
+    // Since YOUR code did not work, PluralSight mr. I refactored it a bit and copied the source input file over
+    // to the correct directory, so It will run even if I would work from another machine.
+    // public string SourceCsvPath => @"c:\src-pluralsight\product-input.csv";
+    // public string TargetCsvPath => @"c:\src-pluralsight\product-output.csv";
+    private const string FileInputName = "product-input.csv"; // File belongs in ./bin/Debug/net8.0 - I also copied it to net6.0 just to make sure.
+    private const string FileOutputName = "product-output.csv";
 
-    public string SourceCsvPath => @"c:\src-pluralsight\product-input.csv";
-    public string TargetCsvPath => @"c:\src-pluralsight\product-output.csv";
+    public string SourceCsvPath => Path.Combine(Directory.GetCurrentDirectory(), FileInputName);
+    public string TargetCsvPath => Path.Combine(Directory.GetCurrentDirectory(), FileOutputName);
 }
